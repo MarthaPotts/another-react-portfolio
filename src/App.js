b3240f6d4fcc6f002655c034282a0af50af9ba98
Martha Potts <marthajpotts7@gmail.com>
2021-07-07 05:10:52 -0400

    test data retrieved successfully

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import { data } from "../public/data.js"
+import React, { useEffect } from 'react';
 
 function App() {
+  function getData() {
+    fetch('data.json', {
+      heaers: {
+        "Content-Type": "application/json", 
+        "Accept": 'application/json'
+      }
+    })
+    .then( (res) => {
+      console.log(res); 
+      return res.json();
+      })
+      .then( (myData) => {
+        console.log(myData); 
+      })
+      .catch((err) => console.log(err)); 
+  };
+
+  useEffect(()=> {
+    getData()
+  }, [])
   return (
     <div className="App">
-    
-    still waiting for functioning components
-    
+
     </div>
   );
 }
